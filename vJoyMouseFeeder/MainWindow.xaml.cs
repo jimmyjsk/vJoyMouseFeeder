6e779699473a4f3ffad71a057aee82f0475109b0
jimmyjsk <medik.jakub+github@gmail.com>
2016-03-11 23:45:53 +0100

    Empty project structure

---
+++ vJoyMouseFeeder/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Point = System.Drawing.Point;
+
+namespace vJoyMouseFeeder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool GetCursorPos(ref Point lpPoint);
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            new Thread(() =>
+            {
+                while (true)
+                {
+                    //Logic
+                    Point p = new Point();
+                    GetCursorPos(ref p);
+
+                    //Update UI
+                    Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        PositionLabel.Content = "X: " + p.X + ", Y:" + p.Y;
+                    }));
+
+                    Thread.Sleep(10);
+                }
+            }).Start();
+        }
+    }
+}
